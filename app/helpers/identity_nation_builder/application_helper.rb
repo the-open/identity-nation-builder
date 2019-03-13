9cd889b8cbfe1a80b0ada0e1e7e9f535451c249b
Joni Gear <joni@getup.org.au>
2019-03-14 10:03:29 +1100

    Include event date, time and timezone on push to events selector

--- app/helpers/identity_nation_builder/application_helper.rb
+++ app/helpers/identity_nation_builder/application_helper.rb
@@ -4,7 +4,7 @@ module IdentityNationBuilder
       [["Event RSVP", :rsvp], ["Tag", :tag]]
     end
     def self.events_for_select
-      IdentityNationBuilder::API.cached_sites_events.map { |x| ["##{x['id']}: #{x['title']}", x['id'], {class: "site_slug_#{x['site_slug']}"}] }
+      IdentityNationBuilder::API.cached_sites_events.map { |x| ["##{x['id']}: #{x['title']} - #{x['start_time'].to_time.strftime('%a %d %b %Y %l:%M%p')} (timezone: #{x['time_zone']})", x['id'], {class: "site_slug_#{x['site_slug']}"}] }
     end
     def self.sites_for_select
       IdentityNationBuilder::API.cached_sites.map { |x| ["#{x['name']}", x['slug']] }
